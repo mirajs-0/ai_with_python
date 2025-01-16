3959dc55a24d25b7b1285de9edcb5b1e6ddae923
mirajs-0 <mirajs@metropolia.fi>
2025-01-16 11:47:29 +0200

    Assignment Q1

---
+++ Assignment 1/1.py
@@ -0,0 +1,17 @@
+def tester(givenstring="Too short"):
+    # Print the default value if length of string is < 10
+    if len(givenstring.replace(" ", "")) < 10:
+        print(tester.__defaults__)
+    else:
+        print (givenstring)
+
+def main():
+    while True:
+        prompt = input("Write something (quit ends): ")
+        if prompt.lower() == "quit":
+            break
+        # Calling tester function
+        tester(prompt)
+
+#Run main function
+main()
