f4f6852a6000e207e6b9bb7aaec8b43603406376
mirajs-0 <mirajs@metropolia.fi>
2025-01-20 12:53:19 +0200

    Assignment Q1

---
+++ Assignment 1/4.py
@@ -0,0 +1,45 @@
+def my_split(sentence, separator=' '):
+    result = []
+    current_word = ''
+
+    for character in sentence:
+        if character == separator:
+            result.append(current_word)
+            current_word = ''
+        else:
+            current_word += character
+
+    # Append the last word if it exists
+    if current_word:
+        result.append(current_word)
+
+    return result
+
+
+def my_join(items, separator=','):
+
+    result = ''
+    for i, item in enumerate(items):
+        result += str(item)
+        if i < len(items) - 1:  #Don't add separator after last item
+            result += separator
+
+    return result
+
+
+# Main program for testing
+def main():
+    sentence = input("Please enter sentence:")
+
+    # Split the sentence into words
+    words = my_split(sentence)
+
+    # Join words with commas and print
+    print(my_join(words, ','))
+
+    # Print each word on a new line
+    for word in words:
+        print(word)
+
+#Run Main Function
+main()
